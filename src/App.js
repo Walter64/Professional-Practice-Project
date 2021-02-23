4e02ea629387e2cd281fdf422cd9ac978eb01ca0
Stephen Welch <stephenwelch1927@yahoo.com>
2021-02-23 18:18:11 +0000

    Worked on App.js, installed react-framework, installedboostrap, added a NavBar

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Navbar, Nav} from'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+    <header>
+      Hello
+    </header>
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+     <Navbar.Brand href="/">World Beers</Navbar.Brand>
+     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+     <Navbar.Collapse id="responsive-navbar-nav">
+       <Nav className="mr-auto">
+         <Nav.Link href="/lookUpHolidayExperience">Link 1</Nav.Link>
+         <Nav.Link href="/addYourOwnExperience">Link 2</Nav.Link>
+         <Nav.Link href="/contactUs">Link 3</Nav.Link>
+       </Nav>
+   
+     </Navbar.Collapse>
+   </Navbar>
+   </div>
+  );
+}
+
+export default App;
