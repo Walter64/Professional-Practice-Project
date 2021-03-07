aee30488ce24972c083f2380a0847a1d5d53acd3
Stephen Welch <stephenwelch1927@yahoo.com>
2021-03-07 21:00:11 +0000

    Fix #3 - Initalize Project

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
     <header>
       Hello
     </header>
+    <h1>Hello Again</h1>
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
      <Navbar.Brand href="/">World Beers</Navbar.Brand>
      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
