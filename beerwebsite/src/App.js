b13475941cf19d3e574eca8c1e96b8d43cba2db4
Stephen Welch <stephenwelch1927@yahoo.com>
2021-03-08 18:58:12 +0000

    Fix For Logo

--- beerwebsite/src/App.js
+++ beerwebsite/src/App.js
@@ -7,10 +7,18 @@ function App() {
   return (
     <div className="App">
     <header>
-      Hello
-      <h1>I Hope this works</h1>
-      <p>I really hope this works!!!!</p>
+    <div className="LogIn">
+  <svg xmlns="http://www.w3.org/2000/svg" width="50" height="40" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+  <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
+</svg>
+<br></br>
+<p>Log In</p>
+</div>
     </header>
+    <br></br>
+    <br></br>
+    <br></br>
+    <div className="Nav">
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
      <Navbar.Brand href="/">World Beers</Navbar.Brand>
      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
@@ -23,6 +31,7 @@ function App() {
    
      </Navbar.Collapse>
    </Navbar>
+   </div>
    </div>
   );
 }
