5a7751256ac12bea1fd15898ac69d424860f36fa
Walter Regan <walterregan@gmail.com>
2021-03-09 15:44:00 +0000

    Fix #7, UI - add cart and text to header and add css styling to cart and navbar

--- beerwebsite/src/App.js
+++ beerwebsite/src/App.js
@@ -1,38 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Navbar, Nav} from'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-    <header>
-    <div className="LogIn">
-  <svg xmlns="http://www.w3.org/2000/svg" width="50" height="40" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
-  <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
-</svg>
-<br></br>
-<p>Log In</p>
-</div>
-    </header>
-    <br></br>
-    <br></br>
-    <br></br>
-    <div className="Nav">
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-     <Navbar.Brand href="/">World Beers</Navbar.Brand>
-     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-     <Navbar.Collapse id="responsive-navbar-nav">
-       <Nav className="mr-auto">
-         <Nav.Link href="/lookUpHolidayExperience">Link 1</Nav.Link>
-         <Nav.Link href="/addYourOwnExperience">Link 2</Nav.Link>
-         <Nav.Link href="/contactUs">Link 3</Nav.Link>
-       </Nav>
-   
-     </Navbar.Collapse>
-   </Navbar>
-   </div>
-   </div>
+      <header>
+        <div>
+          <div id="signIn">            
+            <figure>
+              <svg xmlns="http://www.w3.org/2000/svg" width="50" height="40" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+                <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
+              </svg>
+              <figcaption id="text">Sign In</figcaption>
+            </figure>
+          </div>
+          <div id="cart">
+            <figure>
+              <svg xmlns="http://www.w3.org/2000/svg" width="50" height="40" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+                <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+              </svg>
+              <figcaption>£0.00</figcaption>
+            </figure>
+          </div>
+        </div>
+      </header>
+
+      <div id="nav">
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+          <Navbar.Brand href="/">World Beers</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="/lookUpHolidayExperience">Link 1</Nav.Link>
+              <Nav.Link href="/addYourOwnExperience">Link 2</Nav.Link>
+              <Nav.Link href="/contactUs">Link 3</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
+    </div>
   );
 }
 
