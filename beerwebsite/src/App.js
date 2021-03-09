cb9c41d65865d324c41ab15db7aeec9cdf4a6e7f
Walter Regan <walterregan@gmail.com>
2021-03-09 17:36:59 +0000

    partial commit

--- beerwebsite/src/App.js
+++ beerwebsite/src/App.js
@@ -7,8 +7,19 @@ function App() {
   return (
     <div className="App">
       <header>
-        <div>
-          <div id="signIn">            
+        <div id="header">
+
+          <div class="input-group">
+            <div class="form-outline">
+              <input type="search" id="form1" class="form-control" />
+              
+            </div>
+            <button type="button" class="btn btn-primary">
+              <i class="fas fa-search"></i>
+            </button>
+          </div>
+
+          <div id="signIn">
             <figure>
               <svg xmlns="http://www.w3.org/2000/svg" width="50" height="40" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
                 <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
@@ -16,6 +27,7 @@ function App() {
               <figcaption id="text">Sign In</figcaption>
             </figure>
           </div>
+
           <div id="cart">
             <figure>
               <svg xmlns="http://www.w3.org/2000/svg" width="50" height="40" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
