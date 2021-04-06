34c49e7617ceb88387d63bc4a27096628b1f26d0
Stephen Welch <stephenwelch1927@yahoo.com>
2021-04-06 12:47:06 +0100

    Fix 45 completed login component created with bootstrap form

---
+++ beerwebsite/src/components/cart.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {WorldItem} from '../components/worldItem';
+
+export class Cart extends React.Component{
+    render(){
+        return(
+        <div>
+
+         <h1>hello</h1>
+        </div>
+        
+        );
+    }
+    
+
+}
