c85cc3c2706cdaf0630cdca7ede5b0f9e9951b4e
Stephen Welch <stephenwelch1927@yahoo.com>
2021-04-21 13:47:08 +0100

    Fix for main page carousel function

--- beerwebsite/src/components/mainPage.js
+++ beerwebsite/src/components/mainPage.js
@@ -1,9 +1,53 @@
 import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import garden1 from '../images/garden1.jpg';
+import garden2 from '../images/garden2.jpg';
+import garden3 from '../images/garden3.jpg';
 
 export class mainPage extends React.Component{
     render(){
         return(
-            <h1>Hello From mainPage</h1>
+        <div> 
+            <Carousel fade id="car">
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={garden1}
+      alt="First slide"
+    />
+    <Carousel.Caption>
+      <h3>Beers Anywhere</h3>
+      <p>From all corners of the globe</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={garden3}
+      alt="Second slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Let The Good Times Flow</h3>
+      <p>Check out our competitive rates.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={garden2}
+      alt="Third slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Cheers to Quality Beer</h3>
+      <p>Only the best will do for us.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
+    </div>
         );
+       
     }
+
 }
